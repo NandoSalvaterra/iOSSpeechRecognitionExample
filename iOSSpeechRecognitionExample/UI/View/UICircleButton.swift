d098b0d475b7b5431d7f9056cdf0070e255498b9
Luiz Fernando Salvaterra <luizfernandosalvaterra@gmail.com>
2017-10-03 08:50:36 -0300

    Ajusts microfone icon to show in UIButton.

--- iOSSpeechRecognitionExample/UI/View/UICircleButton.swift
+++ iOSSpeechRecognitionExample/UI/View/UICircleButton.swift
@@ -14,14 +14,21 @@ class UICircleButton: UIButton {
     @IBInspectable var borderColor: UIColor = UIColor.clear
     
     override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
         self.layer.cornerRadius = self.layer.bounds.width / 2
         self.layer.borderColor = borderColor.cgColor
         self.layer.borderWidth = 2.0
+        self.imageView?.contentMode = .scaleAspectFit
+        self.imageEdgeInsets = UIEdgeInsetsMake(12, 12, 12, 12)
     }
     
     override func layoutSubviews() {
+        super.layoutSubviews()
         self.layer.cornerRadius = self.layer.bounds.width / 2
         self.layer.borderColor = borderColor.cgColor
         self.layer.borderWidth = 2.0
+        self.imageView?.contentMode = .scaleAspectFit
+        self.imageEdgeInsets = UIEdgeInsetsMake(12, 12, 12, 12)
+
     }
 }
